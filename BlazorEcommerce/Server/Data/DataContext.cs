acb2964ed099d70ab9c9b2f8ed6bfb499ed1c7f1
takat <takat@SILVERBIRB>
2022-02-07 03:31:25 +0700

    Add project files.

---
+++ BlazorEcommerce/Server/Data/DataContext.cs
@@ -0,0 +1,45 @@
+﻿namespace BlazorEcommerce.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Title = "Book1",
+                    Description = "dsfsdfsdfdsxzfdfsdf",
+                    ImageUrl = "https://livedemo00.template-help.com/wt_prod-19881/images/book-1-210x325.png",
+                    Price = 10
+                },
+
+                new Product
+                {
+                    Id = 2,
+                    Title = "Book2",
+                    Description = "4f324f32sdsasef",
+                    ImageUrl = "https://livedemo00.template-help.com/wt_prod-19881/images/book-2-210x325.png",
+                    Price = 20
+                },
+
+                new Product
+                {
+                    Id = 3,
+                    Title = "Book3",
+                    Description = "432f4fjghjfgrtrtet",
+                    ImageUrl = "https://livedemo00.template-help.com/wt_prod-19881/images/book-3-210x325.png",
+                    Price = 30
+                }
+            );
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ BlazorEcommerce/Server/Migrations/20220206202709_CreateInitial.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorEcommerce.Server.Migrations
+{
+    public partial class CreateInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 1, "dsfsdfsdfdsxzfdfsdf", "https://livedemo00.template-help.com/wt_prod-19881/images/book-1-210x325.png", 10m, "Book1" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 2, "4f324f32sdsasef", "https://livedemo00.template-help.com/wt_prod-19881/images/book-2-210x325.png", 20m, "Book2" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 3, "432f4fjghjfgrtrtet", "https://livedemo00.template-help.com/wt_prod-19881/images/book-3-210x325.png", 30m, "Book3" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
